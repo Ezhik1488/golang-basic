e3774a191cd971e33664733d2b9347bbb8c180c5
Alexandr <alexandr@biznes-len.com>
2025-04-09 11:16:48 +0300

    add functiopn
    Добавлен функция приема ввода от пользователя и пустая функция для расчёта

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,8 +7,32 @@ func main() {
 		USDToEUR = 0.92
 		USDToRUB = 86.19
 	)
-
+	var (
+		firstCurrency, secondCurrency string
+		amount                        float64
+	)
 	EURToRUB := USDToRUB / USDToEUR
 
-	fmt.Println(EURToRUB)
+	fmt.Println("___Конвертер валют___")
+	firstCurrency, secondCurrency, amount = UserInput()
+	fmt.Println(firstCurrency, secondCurrency, amount, EURToRUB)
+
+}
+
+func UserInput() (string, string, float64) {
+	var firstCurrency, secondCurrency string
+	var amount float64
+	fmt.Print("Введите исходную валюту(EUR, USD, RUB): ")
+	fmt.Scan(&firstCurrency)
+	fmt.Print("Введите целевую валюту(EUR, USD, RUB): ")
+	fmt.Scan(&secondCurrency)
+	fmt.Print("Введите сумму для конвертации: ")
+	fmt.Scan(&amount)
+
+	return firstCurrency, secondCurrency, amount
+
+}
+
+func CurrencyConverter(source, target string, amount float64) float64 {
+	return 0.00
 }

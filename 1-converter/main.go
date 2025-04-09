32b232938eff17ce9289c243a156996d84fd39b2
Alexandr <alexandr@biznes-len.com>
2025-04-09 14:54:52 +0300

    complete solution

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,38 +1,100 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	USDToEUR = 0.92
+	USDToRUB = 86.19
+	EURToRUB = 93.78
+)
 
 func main() {
-	const (
-		USDToEUR = 0.92
-		USDToRUB = 86.19
-	)
-	var (
-		firstCurrency, secondCurrency string
-		amount                        float64
-	)
-	EURToRUB := USDToRUB / USDToEUR
+	helloMessage()
+	firstCurrency, secondCurrency, amount := UserInput()
+	result := CurrencyConverter(firstCurrency, secondCurrency, amount)
+	fmt.Printf("Результат конвертации %s в %s: %.2f", firstCurrency, secondCurrency, result)
+}
 
-	fmt.Println("___Конвертер валют___")
-	firstCurrency, secondCurrency, amount = UserInput()
-	fmt.Println(firstCurrency, secondCurrency, amount, EURToRUB)
-
+func helloMessage() {
+	fmt.Println("\tКонвертер валют")
+	fmt.Println("=====================")
+	fmt.Println("Доступные валюты для конвертации: USD, EUR, RUB")
+	fmt.Print("=====================\n\n")
 }
 
 func UserInput() (string, string, float64) {
-	var firstCurrency, secondCurrency string
-	var amount float64
-	fmt.Print("Введите исходную валюту(EUR, USD, RUB): ")
-	fmt.Scan(&firstCurrency)
-	fmt.Print("Введите целевую валюту(EUR, USD, RUB): ")
-	fmt.Scan(&secondCurrency)
-	fmt.Print("Введите сумму для конвертации: ")
-	fmt.Scan(&amount)
+	firstCurrency := inputStringValue("Введите исходную валюту: ", "")
+	secondCurrency := inputStringValue("Введите целевую валюту: ", firstCurrency)
+	amount := inputFloatValue("Введите сумму для конвертации: ")
 
 	return firstCurrency, secondCurrency, amount
 
 }
 
+func inputFloatValue(msg string) float64 {
+	var result float64
+
+	for result <= 0 {
+		fmt.Printf("%s", msg)
+		_, err := fmt.Scan(&result)
+		if err != nil {
+			fmt.Println("Ошибка при получении ввода:" + err.Error())
+			return 0.00
+		}
+		if result <= 0 {
+			fmt.Println("[ОШИБКА] Сумма для конвертации должна быть > 0")
+		}
+	}
+	return result
+}
+
+func inputStringValue(msg, secondCurr string) string {
+	var value string
+	for {
+		fmt.Printf("%s", msg)
+		_, err := fmt.Scan(&value)
+		if err != nil {
+			fmt.Println("Ошибка при получении ввода:" + err.Error())
+			return ""
+		}
+		value = strings.ToLower(value)
+
+		if secondCurr == value {
+			fmt.Println("[ОШИБКА] Выберите разные валюты для конвертации!")
+			continue
+		}
+
+		switch value {
+		case "eur":
+			return value
+		case "usd":
+			return value
+		case "rub":
+			return value
+		default:
+			fmt.Println("[ОШИБКА] Данная валюта недоступна для конвертации. Доступные валюты: USD, EUR, RUB")
+		}
+	}
+}
+
 func CurrencyConverter(source, target string, amount float64) float64 {
-	return 0.00
+	switch {
+	case source == "usd" && target == "eur":
+		return amount * USDToEUR
+	case source == "usd" && target == "rub":
+		return amount * USDToRUB
+	case source == "eur" && target == "rub":
+		return amount * EURToRUB
+	case source == "eur" && target == "usd":
+		return amount / USDToEUR
+	case source == "rub" && target == "usd":
+		return amount / USDToRUB
+	case source == "rub" && target == "eur":
+		return amount / EURToRUB
+	default:
+		return 0.00
+	}
 }

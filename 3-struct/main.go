18c7f7d8c1eeddca4ea944954177f4ae06da6493
Alexandr <alexandr@biznes-len.com>
2025-04-10 15:42:55 +0300

    add constructor func
    Сделаны структуры, а также их конструкторы для создания. Также добавлена валидация данных

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Bin struct {
+	ID        string
+	Name      string
+	Private   bool
+	CreatedAt time.Time
+}
+
+type BinList struct {
+	Bins []Bin
+}
+
+func newBin(id string, name string, private bool) (*Bin, error) {
+	if id == "" {
+		return nil, errors.New("id is required")
+	}
+	if name == "" {
+		return nil, errors.New("name is required")
+	}
+
+	return &Bin{
+		ID:        id,
+		Name:      name,
+		Private:   private,
+		CreatedAt: time.Now(),
+	}, nil
+}
+
+func newBinList() *BinList {
+	return &BinList{}
+}
+
 func main() {
+	bin1, err := newBin("1", "First", false)
+	if err != nil {
+		fmt.Println(err)
+	}
+	binList := newBinList()
+	binList.Bins = append(binList.Bins, *bin1)
+
+	fmt.Println(bin1)
+	fmt.Println(binList)
 
 }
